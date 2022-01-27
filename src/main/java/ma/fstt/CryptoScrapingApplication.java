a8e6b04762003ba69f6fb8153c2bad324ddf8f53
AARABSIM99 <ismael.aarab@etu.uae.ac.ma>
2022-01-28 00:20:28 +0100

    first commit

---
+++ src/main/java/ma/fstt/CryptoScrapingApplication.java
@@ -0,0 +1,13 @@
+package ma.fstt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoScrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoScrapingApplication.class, args);
+	}
+
+}
